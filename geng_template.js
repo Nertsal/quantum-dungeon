6037e723c459fb1384133bde0765332517fe3176
Nertsal <Nertsal@users.noreply.github.com>
2023-10-01 16:36:41 +0000

    deploy: 1b52f7a26854856efbfa355cf86496d5097a47fe

--- geng_template.js
+++ geng_template.js
@@ -1516,16 +1516,16 @@ function __wbg_get_imports() {
         const ret = makeClosure(arg0, arg1, 32, __wbg_adapter_34);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1189 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 504, __wbg_adapter_48);
+    imports.wbg.__wbindgen_closure_wrapper1190 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 506, __wbg_adapter_48);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3685 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 1631, __wbg_adapter_51);
+    imports.wbg.__wbindgen_closure_wrapper3686 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 1633, __wbg_adapter_51);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3718 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 1643, __wbg_adapter_54);
+    imports.wbg.__wbindgen_closure_wrapper3719 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 1645, __wbg_adapter_54);
         return addHeapObject(ret);
     };
     imports['./snippets/geng-3750f959708a1ebc/inline3.js'] = __wbg_star0;
