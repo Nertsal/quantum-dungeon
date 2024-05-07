a3109235499b92c9f730580dadd4ef48c7e7ecc6
nertsal <nestralnertsal@gmail.com>
2024-05-07 20:33:25 +0300

    touch controls

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,4 @@
-use geng::{Key, MouseButton};
+use geng::{Key, MouseButton, Touch};
 
 use crate::{prelude::*, render::GameRender};
 
@@ -8,12 +8,23 @@ pub struct Game {
     render: GameRender,
     model: Model,
     framebuffer_size: vec2<usize>,
+    time: f32,
+
     cursor_pos: vec2<f64>,
     cursor_ui_pos: vec2<f32>,
     cursor_world_pos: vec2<f32>,
     cursor_grid_pos: vec2<f32>,
+
+    active_touch: Option<ActiveTouch>,
     // TODO
     // controls: Controls,
+}
+
+#[derive(Debug, Clone)]
+struct ActiveTouch {
+    start: Touch,
+    start_time: f32,
+    current: Touch,
 }
 
 impl Game {
@@ -29,10 +40,70 @@ impl Game {
             render: GameRender::new(geng, assets, all_items),
             model: Model::new(assets.clone(), config, all_items),
             framebuffer_size: vec2(1, 1),
+            time: 0.0,
+
             cursor_pos: vec2::ZERO,
             cursor_ui_pos: vec2::ZERO,
             cursor_world_pos: vec2::ZERO,
             cursor_grid_pos: vec2::ZERO,
+
+            active_touch: None,
+        }
+    }
+
+    fn handle_lmb(&mut self) {
+        if self.render.inventory_button.contains(self.cursor_ui_pos) {
+            self.render.show_inventory = !self.render.show_inventory;
+            self.assets.sounds.step.play();
+            return;
+        }
+        match self.model.phase {
+            Phase::GameOver => {
+                if self.render.retry_button.contains(self.cursor_ui_pos) {
+                    self.model.player_action(PlayerInput::Retry);
+                }
+            }
+            Phase::Player if self.render.skip_turn_button.contains(self.cursor_ui_pos) => {
+                self.model.player_action(PlayerInput::Skip);
+            }
+            Phase::Select { .. } => {
+                if let Some(i) = self
+                    .render
+                    .buttons
+                    .iter()
+                    .position(|(_, button)| button.contains(self.cursor_ui_pos))
+                {
+                    self.model.player_action(PlayerInput::SelectItem(i));
+                } else if self.render.reroll_button.contains(self.cursor_ui_pos) {
+                    self.model.player_action(PlayerInput::Reroll);
+                } else if self.render.skip_item_button.contains(self.cursor_ui_pos) {
+                    self.model.player_action(PlayerInput::Skip);
+                }
+            }
+            Phase::Vision => {
+                let target = self.cursor_grid_pos.map(|x| x.floor() as Coord);
+                // if self.model.grid.check_pos(target) {
+                self.model.player_action(PlayerInput::Vision {
+                    pos: target,
+                    commit: true,
+                });
+                // }
+            }
+            _ => {
+                let target = self.cursor_grid_pos.map(|x| x.floor() as Coord);
+                self.model.player_action(PlayerInput::Tile(target));
+            }
+        }
+    }
+
+    fn handle_touch_end(&mut self) {
+        let Some(touch) = self.active_touch.take() else {
+            return;
+        };
+
+        if self.time - touch.start_time < 0.5 && touch.start.position == touch.current.position {
+            // Short tap
+            self.handle_lmb();
         }
     }
 }
@@ -91,50 +162,30 @@ impl geng::State for Game {
         }
 
         if geng_utils::key::is_event_press(&event, [MouseButton::Left]) {
-            if self.render.inventory_button.contains(self.cursor_ui_pos) {
-                self.render.show_inventory = !self.render.show_inventory;
-                self.assets.sounds.step.play();
-                return;
+            self.handle_lmb();
+        }
+
+        match event {
+            geng::Event::TouchStart(touch) => {
+                self.active_touch = Some(ActiveTouch {
+                    start: touch,
+                    start_time: self.time,
+                    current: touch,
+                });
             }
-            match self.model.phase {
-                Phase::GameOver => {
-                    if self.render.retry_button.contains(self.cursor_ui_pos) {
-                        self.model.player_action(PlayerInput::Retry);
-                    }
-                }
-                Phase::Player if self.render.skip_turn_button.contains(self.cursor_ui_pos) => {
-                    self.model.player_action(PlayerInput::Skip);
-                }
-                Phase::Select { .. } => {
-                    if let Some(i) = self
-                        .render
-                        .buttons
-                        .iter()
-                        .position(|(_, button)| button.contains(self.cursor_ui_pos))
-                    {
-                        self.model.player_action(PlayerInput::SelectItem(i));
-                    } else if self.render.reroll_button.contains(self.cursor_ui_pos) {
-                        self.model.player_action(PlayerInput::Reroll);
-                    } else if self.render.skip_item_button.contains(self.cursor_ui_pos) {
-                        self.model.player_action(PlayerInput::Skip);
-                    }
-                }
-                Phase::Vision => {
-                    let target = self.cursor_grid_pos.map(|x| x.floor() as Coord);
-                    // if self.model.grid.check_pos(target) {
-                    self.model.player_action(PlayerInput::Vision {
-                        pos: target,
-                        commit: true,
-                    });
-                    // }
-                }
-                _ => {
-                    let target = self.cursor_grid_pos.map(|x| x.floor() as Coord);
-                    // if self.model.grid.check_pos(target) {
-                    self.model.player_action(PlayerInput::Tile(target));
-                    // }
+            geng::Event::TouchMove(touch) => {
+                if let Some(active) = &mut self.active_touch {
+                    active.current = touch;
+                    self.cursor_pos = touch.position;
                 }
             }
+            geng::Event::TouchEnd(touch) => {
+                if let Some(active) = &mut self.active_touch {
+                    active.current = touch;
+                    self.handle_touch_end();
+                }
+            }
+            _ => (),
         }
     }
 
